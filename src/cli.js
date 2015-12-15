b6b6c18dbdb87d35ed0fb8f0372e391b1bebe08c
Alberto Forni <alberto.forn@gmail.com>
2015-12-15 11:27:45 +0000

    Display help message

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,33 @@
+import chalk from 'chalk';
+import parseArgv from 'minimist';
+
+let usage = `
+  usage: pasta [options] [command]
+
+  commands:
+
+    ${ chalk.bold('new') } [name] - create a scaffold project
+    ${ chalk.bold('create') } [template] [name] - create a set of files base on the specified template
+
+    ${ chalk.bold('help') } - Display the available options
+
+  global options:
+
+    -v, --verbose   show debug information
+    -q, --quiet     only output critical errors
+    -V, --version   output version and exit
+    -h, --help      show help
+`
+
 let CLI = {
   main(argv) {
-    console.log(argv);
+    let opts = parseArgv(argv);
+    let cmd = opts._[2];
+
+    if (cmd === 'help' || !cmd) {
+      console.log(usage);
+      process.exit(0);
+    }
   },
 };
 
